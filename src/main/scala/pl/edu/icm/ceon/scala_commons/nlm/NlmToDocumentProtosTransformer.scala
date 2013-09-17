f6b0a36ced05695f66273c6ac299a35cd2044b99
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-09-17 15:33:25 +0200

    imports clean-up

--- src/main/scala/pl/edu/icm/ceon/scala_commons/nlm/NlmToDocumentProtosTransformer.scala
+++ src/main/scala/pl/edu/icm/ceon/scala_commons/nlm/NlmToDocumentProtosTransformer.scala
@@ -2,7 +2,7 @@ package pl.edu.icm.ceon.scala_commons.nlm
 
 import pl.edu.icm.model.transformers.{MetadataFormat, MetadataModel, MetadataReader}
 import pl.edu.icm.coansys.models.DocumentProtos.DocumentWrapper
-import java.io.{PipedOutputStream, PipedInputStream, Reader}
+import java.io.Reader
 import org.apache.commons.io.IOUtils
 import org.apache.commons.io.input.ReaderInputStream
 
